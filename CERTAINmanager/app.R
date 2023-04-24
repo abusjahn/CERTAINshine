140cffd89967111cd76aeb5652e9979d14a43a43
Andreas Busjahn <abusjahn@healthtwist.de>
2023-04-24 10:31:13 +0200

    before removing sidebar

--- CERTAINmanager/app.R
+++ CERTAINmanager/app.R
@@ -21,14 +21,16 @@ ui <- fluidPage(
                value = TRUE),
   fileInput(inputId = 'ct_file',label = 'Select CERTAIN export file',
             accept='.xlsx', width=800),
-  # numericInput("n rows preview", "Rows", value = 5, min = 1, step = 1),
+  numericInput("n rows preview", "Rows", value = 5, min = 1, step = 1),
   # tableOutput("head"),
   textOutput('sheetlist'),
   verbatimTextOutput('sheets'),
-  textInput(inputId = 'sheetpicker', 
-            label = 'Select numbers for sheets to import',
-            width = 300,
-            placeholder = 'e.g. 1:5,9,12:20')#,
+  uiOutput('sheetselector'),
+  actionButton('importbutton',label = 'Import selected'),
+  # reactiveVal('certaindatalist'),
+  textOutput('Imported'),
+  tableOutput('Sheet1')
+#,
   # textOutput()
 )
 
@@ -36,14 +38,9 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   certain_sheets <- reactive({
     req(input$ct_file)
-     sheetnames <- show_excelsheets(input$ct_file$datapath) 
-     paste(seq_along(sheetnames), sheetnames, sep = ': ', collapse='\n')
-  })
-    certain_datalist <- reactive({
-      req(input$ct_file)
-       import_excelsheets(file = input$ct_file$datapath,
-                                             sheets = 1:15,
-                                             skip_codecols = dropcode)   
+     # sheetnames <- 
+       show_excelsheets(input$ct_file$datapath) 
+     # paste(seq_along(sheetnames), sheetnames, sep = ': ', collapse='\n')
   })
   
     hl_sheets <- reactive({
@@ -55,11 +52,37 @@ server <- function(input, output, session) {
       hl_sheets()})
     output$sheets <- renderText({
     certain_sheets()
-    })  
-  # output$head <- renderTable({
-  #   head(data(), input$n)
-}
+    })
+    # 
+    sheetpicked <- reactive({
+      req(input$ct_file)
+checkboxGroupInput(inputId = 'sheetpicker',
+                   label = 'Select sheets to import:',
+                   choices = certain_sheets(),
+                   selected = certain_sheets())
+            # textInput(inputId = 'sheetpicker', 
+      #         label = 'Select numbers for sheets to import',
+      #         width = 300,
+      #         placeholder = 'e.g. 1:5,9,12:20')
+      })
+    output$sheetselector <- 
+      renderUI({
+        sheetpicked()
+      })
+    
+    certaindatalist <- eventReactive(input$importbutton,{
+      import_excelsheets(file = input$ct_file$datapath,
+                         sheets = NULL,#sheetpicked(),
+                         skip_codecols = input$dropcode)
+      })
 
+    output$Imported <- renderText({
+      paste(certaindatalist() |> names())})
+    
+    output$Sheet1 <- renderTable({
+      head(certaindatalist()[[1]], input$n)
+    })
+}
 
 
 
